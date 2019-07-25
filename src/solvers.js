871e8c4803895ef309f88407c851a88a5af8095a
wangsterj <wang.j@berkeley.edu>
2019-07-24 21:57:10 -0700

    Progress to countNRooksSolutions.js

--- src/solvers.js
+++ src/solvers.js
@@ -38,39 +38,79 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  if (n==1) {
+    return 1;
+  }
+  var board = new Board({n:n});
+  var daddyTree = new Tree(board);
+  var nextQueue = new Queue();
+  var masterArr= [];
+  nextQueue.enqueue(daddyTree);
+  var currentNode = daddyTree;
 
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  while (nextQueue.size() > 0) {
+    console.log(nextQueue.size())
+//     if (nextQueue.size() === 4) { debugger; }
+    currentNode = nextQueue.dequeue();
+    var children = this.makeChildren(currentNode, nextQueue, masterArr);
+//     children.forEach(v => nextQueue.enqueue(v));
+  }
+  return masterArr;
+  // create children with potential paths
+  // loop thru each child to end up with non conflicting paths
+  // recurse
+
+  // console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  // return solutionCount;
 };
 
-window.makeChildren = function(tree) {
+window.makeChildren = function(tree, queue, masterArr) {
   var n = tree.size;
   var board = tree.board;
-  for (var i = 0; i < n; i++) {
-    for (var j = 0; j < n; j++) {
-      var newBoard = new Board(board.attributes);
+  var row = tree.row;
+  var col = tree.col;
+  var oldAtt = board.attributes;
+  var newAtt = {
+    0: board.attributes['0'].slice(),
+    1: board.attributes['1'].slice(),
+    2: board.attributes['2'].slice(),
+    n: board.attributes['n']
+  }
+
+  for (var i = row; i < n; i++) {
+    for (var j = col; j < n; j++) {
+      var newBoard = new Board({n:n});
+      var oldAtt = board.attributes;
+      var newAtt = {
+        0: board.attributes['0'].slice(),
+        1: board.attributes['1'].slice(),
+        2: board.attributes['2'].slice(),
+        n: board.attributes['n']
+      }
+      newBoard.attributes = newAtt;
       newBoard.togglePiece(i,j);
       if (!newBoard.hasAnyRooksConflicts()) {
         var newTree = new Tree(newBoard);
-        tree.children.push(newTree);
         newTree.numRooks = tree.numRooks + 1;
+        if (j === n-1) {
+          newTree.row = i+1;
+          newTree.col = 0;
+        } else {
+          newTree.row = i;
+          newTree.col = j+1;
+        }
+        if (newTree.numRooks === n) {
+          masterArr.push(newTree);
+        } else {
+          tree.children.push(newTree);
+          console.log("QUEUEING NEW TREE:", newTree)
+          console.log("QUEUEING NEW TREE:", queue)
+          queue.enqueue(newTree);
+        }
       }
     }
   }
   return tree.children;
-}
-
-// var board = new Board({n:2})
-// var test = new Tree(board);
-// this.makeChildren(test);
-// console.log(test.children.length)
-
-window.Tree = function(board) {
-  this.children = [];
-  this.board = board;
-  this.size = board.rows().length;
-  this.numRooks = 0;
 }
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
@@ -88,3 +128,47 @@ window.countNQueensSolutions = function(n) {
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
+
+
+// TREE DATA STRUCTURE
+var Tree = function(board) {
+  this.children = [];
+  this.board = board;
+  this.size = board.rows().length;
+  this.numRooks = 0;
+  this.row = 0;
+  this.col = 0;
+}
+
+// QUEUE DATA STRUCTURE
+var Queue = function() {
+  this._storage = {};
+  this._start = 0;
+  this._end = 0;
+  };
+
+
+Queue.prototype.enqueue = function(value) {
+  this._storage[this._end++] = value;
+};
+
+Queue.prototype.dequeue = function() {
+  // This does some unnecessary work sometimes. Can you spot why?
+  var result = this._storage[this._start];
+  delete this._storage[this._start];
+
+  this.size() && this._start++;
+
+  return result;
+};
+
+Queue.prototype.size = function() {
+  return this._end - this._start;
+};
+
+
+
+var board = new Board({n:2})
+var test = new Tree(board);
+var res = this.countNRooksSolutions(2);
+// console.log(test.children.length)
